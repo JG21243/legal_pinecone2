89420d6d6709742c49fd78a97b47e1cb57e19c87
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-09-07 23:10:55 +0000

    Update .vscode/settings.json and .next/server/server-reference-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6db003991a0533ff"; }
+/******/ 		__webpack_require__.h = function() { return "974d7806d128e4b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a86bcc66b6108aec.hot-update.js
+++ .next/static/webpack/webpack.a86bcc66b6108aec.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6db003991a0533ff"; }
+/******/ 	__webpack_require__.h = function() { return "974d7806d128e4b5"; }
 /******/ }();
 /******/ 
 /******/ }
